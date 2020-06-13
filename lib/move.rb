2fb537a805593c2ccb5ee353a716cd409153a781
james wu <jwu_nyc@yahoo.com>
2020-06-13 14:44:25 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -6,7 +6,11 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def update_array_at_with(array,index,value)
+#def update_array_at_with(array,index,value)
+#  array[index]=value
+#end
+
+def move(array,index,value)
   array[index]=value
 end
 
